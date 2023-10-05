ea0488acc95499b21331febac9ac9c94080ebdab
TkRudolph <rudolph.malisha@gmail.com>
2023-10-05 06:21:57 -0700

    6-size.c editt

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - program that prints various data types using sizeof operator
+ * main - program that prints various data types using sizeof 
  *
  * Return: 0 (Success)
  */
@@ -14,10 +14,9 @@ int main(void)
 	float e;
 
 	printf("Size of a char: %zu byte(s)\n", sizeof(a));
-
 	printf("Size of an int: %zu bytes(s)\n", sizeof(b));
 	printf("Size of a long int: %zu bytes(s)\n", sizeof(c));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
-	printf("Size of a float: %zu byt(s)\n", sizeof(e));
+	printf("Size of a float: %zu byte(s)\n", sizeof(e));
 	return (0);
 }
