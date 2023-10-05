c6a4f66c2ac272a2496693339cf4162153e75db7
TkRudolph <rudolph.malisha@gmail.com>
2023-10-05 20:35:55 +0200

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints to standard error
+ *
+ * Return: 1 (error msg is success)
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stderr);
+	return (1);
+}
