420fcc20280abcd20cf1657f5325324373361684
TkRudolph <rudolph.malisha@gmail.com>
2023-10-10 14:27:03 +0200

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - program that prints _putchar followed by new line.
+ *
+ * Return:0 (success)
+ */
+
+
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
