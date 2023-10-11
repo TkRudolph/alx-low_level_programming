805a4c72a1f080f5758f0589bd5b0490430cdc23
TkRudolph <rudolph.malisha@gmail.com>
2023-10-11 04:27:38 +0200

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowrcase
+ *
+ * Returns: 1 (success)
+ * Return: 0 (otherwise)
+ * c - ksksa
+ */
+int _islower(int c)
+{
+
+	if (c >= 'a' && c <= 'z')
+	{
+	return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
