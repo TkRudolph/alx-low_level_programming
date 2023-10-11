4bd4684a3a39debfe11feaf260765003f8680da2
TkRudolph <rudolph.malisha@gmail.com>
2023-10-11 06:00:26 +0200

    running otta time

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit
+ * @n: number to be printed
+ *
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	int x;
+
+	if (n < 0)
+	{
+		x = -n % 10;
+	}
+	else
+	{
+		x = n % 10;
+	}
+	_putchar('0' + x);
+	return (x);
+}
