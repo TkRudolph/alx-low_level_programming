710e83aa2b8ae3b762e2c2ebe71bddfff54a4724
TkRudolph <rudolph.malisha@gmail.com>
2023-10-13 06:04:08 -0700

    9-time_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,21 +6,30 @@
  * Return: Always 0.
  */
 
-void times_tables(void)
+void times_table(void)
 {
 	int x;
-	for (x = 0; x <= 10; x++)
+	int y;
+
+	for (x = 0; x <= 9; x++)
 	{
-		int print = 9 * x;
-		if (print < 10)
+		for (y = 0; y <= 9; y++)
 		{
-			_putchar('0' + print);
-		}
-		else
-		{
-			_putchar('0' + print / 10);
-			_putchar('0' + print % 10);
+			int output = x * y;
+
+			if (output < 10)
+			{
+				_putchar('0' + output);
+			}
+			else
+			{
+				_putchar('0' + output / 10);
+				_putchar('0' + output % 10);
+			}
+			_putchar(',');
+			_putchar('.');
+			_putchar('.');
 		}
 		_putchar('\n');
 	}
-}	
+}
