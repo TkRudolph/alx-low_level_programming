22d469006723bdbe7cad117d5f32b82b6fdcc576
TkRudolph <rudolph.malisha@gmail.com>
2023-10-13 07:53:47 -0700

    c is fun is u just give it time

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98.
+ *
+ * @n: number to be printed
+ *
+ * Return: n
+ */
+
+void print_to_98(int n)
+{
+	for (n = 0; n < 9; n++)
+	_p
+	
