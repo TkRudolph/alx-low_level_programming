a02e4f564f77a5a66acc4a8a11a94a4c015ac046
TkRudolph <rudolph.malisha@gmail.com>
2024-01-11 13:16:06 +0200

    file num.0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
- * _puts_recursion - function to print to screen
- * @s: input
- * Return: Always 0 (Success)
+ *_puts_recursion - function to print to the screen
+ *@s: input
+ *Return: always 0
  */
+
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
@@ -11,8 +12,5 @@ void _puts_recursion(char *s)
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-	if (*s == '\0')
-	{
-		_putchar('\n');
-	}
 }
+
